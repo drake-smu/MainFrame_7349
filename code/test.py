4545df1a01694708e48669bb0280287a2e4ed00d
Carson Drake <drakec@smu.edu>
2019-02-02 12:24:02 -0800

    added code

---
+++ code/test.py
@@ -0,0 +1,11 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+#print("Hello World!")
+
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_EAX)
+ciphertext, tag = cipher.encrypt_and_digest(data)
+
+file_out = open("encrypted.bin", "wb")
+[ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
