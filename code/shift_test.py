1db4dbf0a9dd52f2dfef541cdbe17c561cdaafe7
Carson Drake <drakec@smu.edu>
2019-02-03 03:25:15 -0800

    fixed shifter, outputs are strings but can be converted

--- code/shift_test.py
+++ code/shift_test.py
@@ -1,14 +1,14 @@
 # make generic function
 
 def make_permutations(block_size):
-    print(bin(0).zfill(block_size))
-    for i in range(block_size-2):
+    print(bin(0)[2:].zfill(block_size))
+    for i in range(block_size):
         #print(bin(1<<i).zfill(block_size))
         if(i>0):
             for j in range(i):
-                print(bin(1<<i|1<<j).zfill(block_size))
+                print(bin(1<<i|1<<j)[2:].zfill(block_size))
         else:
-            for k in range(block_size-2):
-                print(bin(1<<k).zfill(block_size))
+            for k in range(block_size):
+                print(bin(1<<k)[2:].zfill(block_size))
         print('-----cycle: %s-----'%(i))
 make_permutations(32)
