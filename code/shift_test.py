05f3b461f231f6c779d81ebcbb45c765f545f808
Carson Drake <drakec@smu.edu>
2019-02-03 01:04:36 -0800

    extended loop

--- code/shift_test.py
+++ code/shift_test.py
@@ -3,6 +3,11 @@
 def make_permutations(block_size):
     print(bin(0).zfill(block_size))
     for i in range(block_size-2):
-        print(bin(1<<i).zfill(block_size))
-
-make_permutations(10)
+        #print(bin(1<<i).zfill(block_size))
+        if(i>0):
+            for j in range(i):
+                print(bin(1<<i|1<<j).zfill(block_size))
+        else:
+            print(bin(1<<i).zfill(block_size))
+        print('-----cycle: %s-----'%(i))
+make_permutations(32)
