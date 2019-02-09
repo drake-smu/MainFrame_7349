07913fabd209094104800324c3b82a6f8c1929ac
Carson Drake <drakec@smu.edu>
2019-02-08 22:26:36 -0800

    fixed bad code

--- code/shift_test.py
+++ code/shift_test.py
@@ -6,7 +6,7 @@ def toggleBit(int_type, offset):
     return(int_type ^mask)
 
 
-def high_denstiy_generator(block_size):
+def high_density_generator(block_size):
     """
     This function makes array of high density 
     strings of size block_size
@@ -15,9 +15,8 @@ def high_denstiy_generator(block_size):
     resp = []
     for block in low_density:
         temp = ''
-        for val in enumerate(block): 
+        for val in block: 
             temp+= bin(toggleBit(int(val,2),0))[2:]
-    
         resp.append(temp)
     return resp
 
@@ -27,24 +26,19 @@ def low_density_generator(block_size):
     This function makes array of low density 
     strings of size block_size
     """
-    # print(bin(0)[2:].zfill(block_size))
     resp = [bin(0)[2:].zfill(block_size)]
 
     for i in range(block_size):
-        #print(bin(1<<i).zfill(block_size))
         if(i>0):
             for j in range(i):
-                # print(bin(1<<i|1<<j)[2:].zfill(block_size))
                 resp.append(bin(1<<i|1<<j)[2:].zfill(block_size))
         else:
             for k in range(block_size):
-                # print(bin(1<<k)[2:].zfill(block_size))
                 resp.append(bin(1<<k)[2:].zfill(block_size))
         # print('-----cycle: %s-----'%(i))
-
     return resp
 
-test = high_denstiy_generator(30)   # High Tensity Test
+test = high_density_generator(30)   # High Tensity Test
 # test = low_density_generator(30)    # Low Density Test
 
 pprint.pprint(test)
