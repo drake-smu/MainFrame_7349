a4f44fb6a813fa06019232d01e87415df471d3b6
Carson Drake <drakec@smu.edu>
2019-02-08 22:17:51 -0800

    converted shift test to have two functions, 1 for high desity text generation and one for low:

--- code/shift_test.py
+++ code/shift_test.py
@@ -1,14 +1,50 @@
-# make generic function
+import pprint
 
-def make_permutations(block_size):
-    print(bin(0)[2:].zfill(block_size))
+
+def toggleBit(int_type, offset):
+    mask = 1 << offset
+    return(int_type ^mask)
+
+
+def high_denstiy_generator(block_size):
+    """
+    This function makes array of high density 
+    strings of size block_size
+    """
+    low_density= low_density_generator(block_size)
+    resp = []
+    for block in low_density:
+        temp = ''
+        for val in enumerate(block): 
+            temp+= bin(toggleBit(int(val,2),0))[2:]
+    
+        resp.append(temp)
+    return resp
+
+
+def low_density_generator(block_size):
+    """
+    This function makes array of low density 
+    strings of size block_size
+    """
+    # print(bin(0)[2:].zfill(block_size))
+    resp = [bin(0)[2:].zfill(block_size)]
+
     for i in range(block_size):
         #print(bin(1<<i).zfill(block_size))
         if(i>0):
             for j in range(i):
-                print(bin(1<<i|1<<j)[2:].zfill(block_size))
+                # print(bin(1<<i|1<<j)[2:].zfill(block_size))
+                resp.append(bin(1<<i|1<<j)[2:].zfill(block_size))
         else:
             for k in range(block_size):
-                print(bin(1<<k)[2:].zfill(block_size))
-        print('-----cycle: %s-----'%(i))
-make_permutations(32)
+                # print(bin(1<<k)[2:].zfill(block_size))
+                resp.append(bin(1<<k)[2:].zfill(block_size))
+        # print('-----cycle: %s-----'%(i))
+
+    return resp
+
+test = high_denstiy_generator(30)   # High Tensity Test
+# test = low_density_generator(30)    # Low Density Test
+
+pprint.pprint(test)
