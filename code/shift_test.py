4e7dc74a4b48a616438a8d38df17b8d0aa9732a2
josephsdavid <josephsd@smu.edu>
2019-03-19 20:52:41 -0500

    high density key

--- code/shift_test.py
+++ code/shift_test.py
@@ -8,14 +8,14 @@ def toggleBit(int_type, offset):
 
 def high_density_generator(block_size):
     """
-    This function makes array of high density 
+    This function makes array of high density
     strings of size block_size
     """
     low_density= low_density_generator(block_size)
     resp = []
     for block in low_density:
         # temp = ''
-        # for val in block: 
+        # for val in block:
         #     temp+= bin(toggleBit(int(val,2),0))[2:]
         temp = ''.join(map(lambda x: bin(toggleBit(int(x,2),0))[2:],block))
         resp.append(temp)
@@ -24,7 +24,7 @@ def high_density_generator(block_size):
 
 def low_density_generator(block_size):
     """
-    This function makes array of low density 
+    This function makes array of low density
     strings of size block_size
     """
     resp = [bin(0)[2:].zfill(block_size)]
@@ -39,7 +39,7 @@ def low_density_generator(block_size):
         # print('-----cycle: %s-----'%(i))
     return resp
 
-# test = high_density_generator(30)   # High Tensity Test
-# test = low_density_generator(30)    # Low Density Test
+#test = high_density_generator(30)   # High Tensity Test
+test = low_density_generator(30)    # Low Density Test
 
-# pprint.pprint(test)
+pprint.pprint(test)
