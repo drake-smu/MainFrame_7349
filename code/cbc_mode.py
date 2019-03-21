88f705fbe7ea666f56a1adae91ec7264b81ad23a
josephsdavid <josephsd@smu.edu>
2019-03-20 19:49:24 -0500

    pep 8 CBC code

--- code/cbc_mode.py
+++ code/cbc_mode.py
@@ -1,9 +1,5 @@
 from Crypto.Cipher import AES
-import random
-import string
 from Crypto.Random import get_random_bytes
-import bitarray
-
 
 
 def aes_cbc_encrypt(IV, key, data, mode=AES.MODE_CBC):
@@ -19,8 +15,7 @@ def loopfun(n=16):
     for i in range(n):
         CT = (aes_cbc_encrypt(IV=IV, key=key, data=txt))
         print(CT)
-        # Help!!! How do I get this fixed? Must make the ciphertext
         IV = (CT)
 
 
-(loopfun(n = 128))
+(loopfun(n=128))
