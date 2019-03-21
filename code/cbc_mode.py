39637d14655682c809f6e8112cae84eb94174b40
josephsdavid <josephsd@smu.edu>
2019-03-20 19:40:04 -0500

    almost have CBC working, need to fix bytes

--- code/cbc_mode.py
+++ code/cbc_mode.py
@@ -2,11 +2,24 @@ from Crypto.Cipher import AES
 import random
 import string
 from Crypto.Random import get_random_bytes
+import bitarray
 
-key = get_random_bytes(16)
-txt = str().zfill(128)
-IV = "0"*128
-# print(IV)
-# print(txt)
-# print(key)
 
+
+def aes_cbc_encrypt(IV, key, data, mode=AES.MODE_CBC):
+    aes = AES.new(key, mode, IV)
+    new_data = aes.encrypt(data)
+    return new_data
+
+
+def loopfun(n=16):
+    key = get_random_bytes(16)
+    txt = str().zfill(128)
+    IV = "0"*16
+    for i in range(n):
+        CT = (aes_cbc_encrypt(IV=IV, key=key, data=txt))
+        print(CT)
+        IV = bitarray.bitarray(CT)
+
+
+print(loopfun(2))
