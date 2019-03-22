b31639de246115e25cfb4460660acd29e213c8c9
josephsdavid <josephsd@smu.edu>
2019-03-22 09:27:40 -0500

    shell skeleton for creating blocks=complete!

--- code/cbc.sh
+++ code/cbc.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #python cbc_mode.py > ~/tmp/tmp.txt
 #sed "s/'/ /g" ~/tmp/tmp.txt
-python cbc_mode.py | sed "s/'/ /g" | sed 's/^.\{2\}//'| awk '{a=a s $0;s=""}END{print a}'
+rm ~/tmp/cbc.input
+for ((i=0;i<=30000;i++)); do
+    python cbc_mode.py  >> ~/tmp/cbc.input
+done
+cat ~/tmp/cbc.input |  sed "s/'/ /g" | sed 's/^.\{2\}//'| awk '{a=a s $0;s=""}END{print a}' | dieharder -a
